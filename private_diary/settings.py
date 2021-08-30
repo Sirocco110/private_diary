e470743a7e3e1e3a9d5daf536e7af95d4ae75b5a
Sirocco110 <hiroki1100410@gmail.com>
2021-08-30 15:11:33 +0900

    create top page

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -145,6 +146,12 @@ LOGGING = {
         },
     }
 }
+
+STATICFILES_DIRS = (
+    
+    os.path.join(BASE_DIR,"static"),
+
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
