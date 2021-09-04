d36c3c2d6bf22fd88798f39050ffaaa4cdc20c04
Sirocco110 <hiroki1100410@gmail.com>
2021-09-04 14:20:06 +0900

    4th deploy check

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -61,5 +61,6 @@ LOGGING = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
