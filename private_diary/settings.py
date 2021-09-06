de0b23426f048fb7f63d9e9fc3887cab6de48613
Sirocco110 <hiroki1100410@gmail.com>
2021-09-06 23:12:06 +0900

    11th deploy2

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -19,13 +19,13 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 
